0ecd90907e88c56808e2e4001c45a7175c537915
yaoazhen@hotmail.com <yaoazhen@gmail.com>
2011-05-22 21:19:39 +0800

    a

---
+++ placeholder.js
@@ -0,0 +1,30 @@
+/**
+ * @author Zane Yao
+ */
+
+// This adds 'placeholder' to the items listed in the jQuery .support object. 
+jQuery(function() {
+   jQuery.support.placeholder = false;
+   test = document.createElement('input');
+   if('placeholder' in test) jQuery.support.placeholder = true;
+});
+// This adds placeholder support to browsers that wouldn't otherwise support it. 
+jQuery(function() {
+   if(!$.support.placeholder) { 
+      var active = document.activeElement;
+      $('[placeholder]:text').focus(function () {
+         if ($(this).attr('placeholder') != '' && $(this).val() == $(this).attr('placeholder')) {
+            $(this).val('').removeClass('hasPlaceholder');
+         }
+      }).blur(function () {
+         if ($(this).attr('placeholder') != '' && ($(this).val() == '' || $(this).val() == $(this).attr('placeholder'))) {
+            $(this).val($(this).attr('placeholder')).addClass('hasPlaceholder');
+         }
+      });
+      $('[placeholder]:text').blur();
+      $(active).focus();
+      $('[placeholder]').parents('form').submit(function() {
+         $(':text.hasPlaceholder').val('');
+      });
+   }
+});
