79c5a3e1107c98e0700907d902ea90da583a3e22
yaoazhen@hotmail.com <yaoazhen@gmail.com>
2011-05-30 23:11:14 +0800

    Change the runtime of support code to beginning, not document ready

--- placeholder.js
+++ placeholder.js
@@ -2,29 +2,37 @@
  * @author Zane Yao
  */
 
-// This adds 'placeholder' to the items listed in the jQuery .support object. 
-jQuery(function() {
+/// <reference path="jquery-1.4.2.js" />
+(function ($) {
+	// This adds 'placeholder' to the items listed in the jQuery .support object. 
    jQuery.support.placeholder = false;
    test = document.createElement('input');
    if('placeholder' in test) jQuery.support.placeholder = true;
-});
-// This adds placeholder support to browsers that wouldn't otherwise support it. 
-jQuery(function() {
-   if(!$.support.placeholder) { 
-      var active = document.activeElement;
-      $('[placeholder]:text').add('textarea[placeholder]').focus(function () {
-         if ($(this).attr('placeholder') != '' && $(this).val() == $(this).attr('placeholder')) {
-            $(this).val('').removeClass('hasPlaceholder');
-         }
-      }).blur(function () {
-         if ($(this).attr('placeholder') != '' && ($(this).val() == '' || $(this).val() == $(this).attr('placeholder'))) {
-            $(this).val($(this).attr('placeholder')).addClass('hasPlaceholder');
-         }
-      });
-      $('[placeholder]:text').add('textarea[placeholder]').blur();
-      $(active).focus();
-      $('[placeholder]').parents('form').submit(function() {
-         $(':text.hasPlaceholder').val('');
-      });
-   }
-});
+	// This adds placeholder support to browsers that wouldn't otherwise support it. 
+	
+	$(function() {
+	   if(!$.support.placeholder) {
+	      var active = document.activeElement;
+	      
+	      var $textboxList=$('[placeholder]:text').add('textarea[placeholder]');
+	      $textboxList.each(function(){
+	      	$.data(this, 'placeholder', $(this).attr('placeholder'));
+	      });
+	      
+	      $textboxList.focus(function () {
+		         if ($.data(this,'placeholder') != '' && $(this).val() == $.data(this,'placeholder')) {
+		            $(this).val('').removeClass('hasPlaceholder');
+		         }
+		     }).blur(function(){
+		         if ($.data(this,'placeholder') != '' && ($(this).val() == '' || $(this).val() == $.data(this,'placeholder'))) {
+		            $(this).val($.data(this,'placeholder')).addClass('hasPlaceholder');
+		         }
+		      });
+	      $textboxList.blur();
+	      $(active).focus();
+	      $('[placeholder]').parents('form').submit(function() {
+	         $(':text.hasPlaceholder').val('');
+	      });
+	   }
+	});
+}(jQuery));
